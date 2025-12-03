dc53d5860bca9b46a14db11ea271fb4e0a30e2c5
thurcruz <arthurindcruz@gmail.com>
2025-12-03 13:56:29 -0300

    gestor system

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Simplesmente redireciona para a página principal (que agora é home.php)
-// A verificação de login ocorre dentro de home.php.
-header('Location: home.php');
-exit();
-?>
